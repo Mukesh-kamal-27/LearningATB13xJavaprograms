5d8e1abf745b4cc1eca371f21848f0fbd504b4c4
mukesh-kamal-27 <mukeshkamal2704@gmail.com>
2025-06-10 00:26:49 +0530

    This is 09th june  practice

---
+++ src/Ex_01_Java_Basics/Comments.java
@@ -0,0 +1,11 @@
+package Ex_01_Java_Basics;
+
+public class Comments {
+    public static void main(String[] args) {
+        /*Author:mukesh kamal
+         Pratice: java Basic
+         Batch:ATB13xautomation*/
+
+        System.out.println("This is comment practice");
+    }
+}
---
+++ src/Ex_01_Java_Basics/Mukesh$java.java
@@ -0,0 +1,7 @@
+package Ex_01_Java_Basics;
+
+public class Mukesh$java {
+    public static void main(String[] args) {
+        System.out.println("Hello 2nd class");
+    }
+}
