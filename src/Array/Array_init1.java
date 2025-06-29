292b66e63d64759277210cc1487acb41f13132b6
Mukesh-kamal-27 <mukeshkamal2704@gmail.com>
2025-06-29 11:13:31 +0530

    New file

---
+++ src/Array/Array_init1.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array_init1 {
+    public static void main(String[] args) {
+        int [] arr = {23,24,67,89};
+        for(int i=0; i<=3;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
